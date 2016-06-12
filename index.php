12e79c218751c3cfeef1c46f9461b123aa23b534
Jonathan Melendez <darkalexve@gmail.com>
2016-06-11 22:28:34 -0400

    text

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 	<center>
 	<div class="box">
 	<img src="images/sicitem.png" alt="Sicitem Logo" style="width:500px;height:300px;" align="center">
+	<h3>Sistema de control e Inventario de Telefonia Movil</h3>
 		<center><div class="Ingreso">
 
 	<table border="0" align="center" valign="middle">
